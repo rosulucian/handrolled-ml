6f9f26d7e3a425474aab57002cbf02c13ae91eb1
rosulucian <rosu.lucian@gmail.com>
2020-05-12 15:20:50 +0300

    nn intermediate2

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils.activations import activations
+from utils.activations import activations, derivatives
 
 
 class neural_network():
@@ -19,7 +19,7 @@ class neural_network():
         # sanity check
         assert(X.shape[1] != 0 and X.shape[1] == self.m)
 
-        self.layers.insert(0, (self.m, 'linear'))  # insert input layer
+        self.layers.insert(0, (self.m, 'identity'))  # insert input layer
         self.layers.append((1, 'sigmoid'))  # add output layer; dummy
 
         for i in range(1, len(self.layers)):
@@ -35,37 +35,69 @@ class neural_network():
 
             self.params.append(layer)
 
-        def f_prop(self, X):
-            activations = [X]
-            # A = X
+    def f_prop(self, X):
+        activations = [{'A': X, 'Z': None}]
 
-            for l in range(1, len(self.layers)):
-                A_prev = activations[l-1]
-                layer = self.params[l]
+        for l in range(1, len(self.layers)):
+            A_prev = activations[l-1].A
+            layer = self.params[l]
 
-                Z = np.dot(layer.W, A_prev) + layer.b
-                A = activations[layer.act](Z)
-                activations.append(A)
+            Z = np.dot(layer.W, A_prev) + layer.b
+            A = activations[layer.act](Z)
+            activations.append({'A': A, 'Z': Z})
 
-            return activations
+        return activations
 
-        def compute_cost(self, AL, Y):
-            J = -np.sum(Y * np.log(AL) + (1-Y) * np.log(1-AL))/self.m
-            return J
+    def compute_cost(self, AL, Y):
+        J = -np.sum(Y * np.log(AL) + (1-Y) * np.log(1-AL))/self.m
+        return J
 
-        def b_prop(self, activations, Y):
-            m = self.m
+    def b_prop(self, activations, Y):
+        m = self.m
+        L = len(self.layers)
 
-        def fit(self, X, Y):
-            self.init_params(X, Y)
+        grads = []
 
-            for i in range(self.max_iter):
-                activations = self.f_prop(X)
+        AL = activations[-1][0]
+        dA_prev = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))  # dAL
 
-                # compute cost
-                cost = self.compute_cost(activations[-1], Y)
+        for l in reversed(range(1, L-1)):
+            layer = self.params[l]
 
-                # back prop
-                self.b_prop(activations, Y)
+            Z = activations[l].Z
+            A_prev = activations[l-1].A
 
-                # update params
+            dA = dA_prev
+            dZ = derivatives[layer.act](dA, Z)
+            dW = np.dot(dZ, A_prev.T)/m
+            db = np.sum(dZ, axis=1, keepdims=True)/m
+
+            dA_prev = np.dot(layer.W.T, dZ)  # for next calculation
+
+        return grads
+
+    def update_params(self, grads):
+        #
+
+        None
+
+    def fit(self, X, Y):
+        self.init_params(X, Y)
+
+        for i in range(self.max_iter):
+            activations = self.f_prop(X)
+
+            # compute cost
+            AL = activations[-1].A
+            cost = self.compute_cost(AL, Y)
+
+            # back prop
+            self.b_prop(activations, Y)
+
+            # update params
+
+    def predict(self, X):
+        None
+
+
+# TODO: add checks everywhere
--- utils/activations.py
+++ utils/activations.py
@@ -7,14 +7,49 @@ activations = {
     'tanh': tanh
 }
 
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-
-def relu(z):
-    return np.maximum(0, z)
+derivatives = {
+    'sigmoid': sigmoid_deriv,
+    'relu': relu_deriv,
+    'tanh': tanh_deriv
+}
 
 
-def tanh(z):
-    return np.tanh(z)
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+
+def relu(Z):
+    return np.maximum(0, Z)
+
+
+def tanh(Z):
+    return np.tanh(Z)
+
+
+def sigmoid_deriv(dA, Z):
+    s = 1/(1+np.exp(-Z))
+    deriv = s * (1-s)
+
+    dZ = dA * deriv
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def relu_deriv(dA, Z):
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def tanh_deriv(dA, Z):
+    A = tanh(Z)
+    dZ = dA * (1 - np.power(A, 2))
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
